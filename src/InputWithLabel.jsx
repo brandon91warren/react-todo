16480ac95a4d01f79e68c68375d8d6e36d4be583
brandon91warren <brandon91warren@github.com>
2024-07-31 00:32:22 -0700

    Add Remove Button to List Items

--- src/InputWithLabel.jsx
+++ src/InputWithLabel.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useRef } from "react";
+
 function InputWithLabel(props) {
+    // useRef used to store DOM elements in state
+    // Does not cause re-renders unlink variables from useState
+    const inputRef = useRef();
+    
+    console.log(inputRef.current);
+
+    useEffect(() => {
+        inputRef.current.focus();
+    });
+
     return(
         <>
         <label htmlFor="ToDoType">{props.children}</label>
@@ -8,8 +20,9 @@ function InputWithLabel(props) {
         placeholder="Add new todo"
         value={props.todoTitle}
         onChange={props.handleTitleChange}
+        ref={inputRef}
       />
-       </>
-    )
+    </>
+    );
 }
 export default InputWithLabel;
