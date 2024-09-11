476638c6528a33054beee1e9b343d058e6653179
brandon91warren <brandon91warren@github.com>
2024-09-10 21:38:35 -0700

    Lesson 3.1: CSS Styled Components SVG

--- src/todolistitem.jsx
+++ src/todolistitem.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './TodoListItem.module.css'; // Import CSS module
 
 export default function TodoListItem({ title, onRemoveTodo, id }) {
   return (
-    <li>
+    <li className={style.ListItem}>
       {title} <button onClick={() => onRemoveTodo(id)}>Remove</button>
     </li>
   );
